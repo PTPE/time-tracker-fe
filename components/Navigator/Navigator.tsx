4daccd9f7003f8522b51d3c6a45a315dbb1c9cef
PTPE <winnie524722@gmail.com>
2026-04-13 20:52:16 +0800

    feat: [TT-8] add desktop navigator

---
+++ components/Navigator/Navigator.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import clsx from "clsx";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import LogoIcon from "@/assets/logo.svg";
+
+const routeConfig = [
+  {
+    name: "Projects",
+    path: "/projects",
+  },
+  {
+    name: "Timer",
+    path: "/timer",
+  },
+  {
+    name: "History",
+    path: "/history",
+  },
+  {
+    name: "Settings",
+    path: "/settings",
+  },
+];
+
+export default function Navigator() {
+  const currentPath = usePathname();
+
+  return (
+    <div className="bg-surface-primary sticky top-0 z-50 flex items-center justify-between px-6 py-4 shadow-sm">
+      <Link className="flex shrink-0 items-center gap-2" href="/projects">
+        <div className="bg-teal flex h-10 w-10 items-center justify-center rounded-lg">
+          <LogoIcon className="h-6 w-6 text-white" />
+        </div>
+        <span className="text-lg leading-tight">
+          Time
+          <br />
+          Tracker
+        </span>
+      </Link>
+      <div className="flex gap-2">
+        {routeConfig.map((route) => (
+          <Link
+            href={route.path}
+            key={route.name}
+            className={clsx(
+              currentPath === route.path
+                ? "bg-teal hover:bg-teal-hover text-white"
+                : "hover:bg-teal-hover text-text-primary hover:text-white",
+              "rounded-lg px-4 py-2 text-sm",
+            )}
+          >
+            {route.name}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
