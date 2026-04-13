3622599a6ee82736c7fa0208de79442822913800
PTPE <winnie524722@gmail.com>
2026-04-13 21:03:59 +0800

    fix: [TT-8] redirect / to /timer with or without access or refresh tokens

--- proxy.ts
+++ proxy.ts
@@ -10,7 +10,7 @@ export function proxy(request: NextRequest) {
 
   const isPublicPath = publicPaths.some((path) => pathname.startsWith(path));
 
-  if ((accessToken || refreshToken) && pathname === "/") {
+  if (pathname === "/") {
     return NextResponse.redirect(new URL("/timer", request.url));
   }
 
